9cdff2778e6ff24b052810f3ba9cc6140532c36e
sho-ki <shoki0116.highjump@gmail.com>
2021-10-14 01:51:10 +0900

    fixded

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -18,9 +18,14 @@ module.exports = {
     try {
       const username = req.body.username;
       const password = req.body.password;
+
       const result = await LoginModel.signIn(username, password);
 
-      res.status(200).json({ result });
+      if (result === 'NO USERS FOUND') {
+        res.status(400).json({ result });
+      } else {
+        res.status(200).json({ result });
+      }
     } catch (e) {
       res.status(500).send({ e });
     }
